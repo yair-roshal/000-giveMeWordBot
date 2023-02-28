36a4c6a818485cf0a70a05ff3ec6d6417819c257
yair-roshal <yair7705778@gmail.com>
2023-02-28 13:18:23 +0200

    fix

--- utils/getPathToFolder.js
+++ utils/getPathToFolder.js
@@ -1,9 +1,14 @@
 module.exports = function getPathToFolder(folder) {
     if (process.env.NODE_ENV === 'dev') {
+        console.log(
+            '`dev-----`',
+            `/Users/yair/Desktop/dev/000-giveMeWordBot/${folder}/`,
+        )
         return `/Users/yair/Desktop/dev/000-giveMeWordBot/${folder}/`
     }
 
     if (process.env.NODE_ENV === 'prod') {
+        console.log('`prod-----`', `${folder}`)
         return `${folder}`
         // return '../data/logs/'
     }
