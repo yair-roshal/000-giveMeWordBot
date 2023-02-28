39f7181d18a55a65429b5bbc25e8fef168b630cb
yair-roshal <yair7705778@gmail.com>
2023-02-28 14:52:02 +0200

    fix

--- utils/dictionaryTextFromFile.js
+++ utils/dictionaryTextFromFile.js
@@ -5,6 +5,15 @@ module.exports = function dictionaryTextFromFile(nameFile) {
     nameFile = 'allWords.txt'
     let path = getPathToFolder('data/')
 
+    console.log(
+        'path.join(__dirname, `../${path}${nameFile}`)',
+        path.join(__dirname, `../${path}${nameFile}`),
+    )
+
+    console.log(' __dirname, `)', __dirname)
+
+    console.log('  `../${path}${nameFile}`)', `../${path}${nameFile}`)
+
     try {
         // var templateContent = fs.readFileSync(path.join(__dirname, '../templates') + '/my-template.html', 'utf8');
 
