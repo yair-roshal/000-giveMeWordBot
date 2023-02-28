0b76aa4071d0b6423259565abe4cf5a45a2aaac8
yair-roshal <yair7705778@gmail.com>
2023-02-28 21:29:26 +0200

    fix

--- utils/dictionaryTextFromFile.js
+++ utils/dictionaryTextFromFile.js
@@ -5,12 +5,12 @@ const checkExistFile = require('./checkExistFile')
 module.exports = function dictionaryTextFromFile() {
     let nameFile = 'allWords.txt'
     let pathFile = getPathToFolder('data/') + `${nameFile}`
-    console.log('pathFile4444', pathFile)
+    // console.log('pathFile4444', pathFile)
     //  checkExistFile(pathFile)
 
     try {
         const data = fs.readFileSync(pathFile, 'utf8')
-        console.log('data999', data)
+        // console.log('data999', data)
         return data
     } catch (err) {
         console.error(
