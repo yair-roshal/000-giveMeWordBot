75f7d29917bd4db26b6b9d12cf7d7da071a726c8
yair-roshal <yair7705778@gmail.com>
2023-02-28 12:57:00 +0200

    fix

--- constants/constants.js
+++ constants/constants.js
@@ -1,3 +1,4 @@
+  
 const startMenu = {
     reply_markup: {
         keyboard: [
--- utils/logSessions.js
+++ utils/logSessions.js
@@ -1,10 +1,15 @@
 let fs = require('fs')
 let appendFile = require('node:fs')
 const getPathToFolder = require('./getPathToFolder')
+const formatDate = require('./formatDate.js')
 
 module.exports = function dictionaryTextToFile() {
     let nameFile = 'log-sessions.txt'
-    let lineText = Date(Date.now()).toString() + '\r\n'
+
+    const timestamp = Date.now()
+    const formattedDate = formatDate(timestamp)
+    let lineText = formattedDate + '\r\n'
+
     let path = getPathToFolder('data/logs')
     console.log('path_dictionaryTextToFile : ', path)
 
