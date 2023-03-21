e0b1d95b524000f440bbc1ffc10499fe83e0b033
yair-roshal <yair7705778@gmail.com>
2023-03-21 11:40:38 +0200

    fix

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: 'give_me_word',
+            script: 'server.js',
+            watch: true,
+            ignore_watch: ['*.txt', 'data/', 'data/cache_allWords.txt'],
+        },
+    ],
+}
+
+//   {
+// 	name   : "app1",
+// 	script : "./app.js",
+// 	env_production: {
+// 	   NODE_ENV: "production"
+// 	},
+// 	env_development: {
+// 	   NODE_ENV: "development"
+// 	}
+//   }
