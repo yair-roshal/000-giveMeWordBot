b1be6840cbe13dc4f40583f8f44dc29fcb2104d5
yair-roshal <yair7705778@gmail.com>
2023-04-25 20:31:13 +0300

    fix

--- utils/refreshTokenIAM.js
+++ utils/refreshTokenIAM.js
@@ -11,18 +11,19 @@ module.exports = async function refreshTokenIAM() {
         jwt: tokenJWT,
     }
 
-    console.log('jwtObj :>> ', jwtObj)
+    // console.log('jwtObj :>> ', jwtObj)
     try {
         const result = await axios
             .post('https://iam.api.cloud.yandex.net/iam/v1/tokens', jwtObj)
             .then((response) => {
                 let IAM_TOKEN = response.data.iamToken
-                console.log('IAM_TOKEN==', { IAM_TOKEN })
+                console.log('IAM_TOKEN==',  Boolean(IAM_TOKEN)  )
+                // console.log('IAM_TOKEN==', { IAM_TOKEN })
                 return IAM_TOKEN
             })
         return result
     } catch (err) {
         logAlerts(err)
-        console.log('AXIOS ERROR _ changeTokenToIAM: ', err.response)
+        console.log('AXIOS ERROR _ refreshTokenIAM: ', err.response)
     }
 }
--- utils/translateText.js
+++ utils/translateText.js
@@ -3,7 +3,6 @@ const {
     source_language,
     target_language,
 } = require('../constants/languages.js')
-const logAlerts = require('./logAlerts')
 const refreshTokenIAM = require('./refreshTokenIAM')
 
 const sleep = (ms) => {
@@ -12,33 +11,20 @@ const sleep = (ms) => {
 
 const request_retry = async (url, body, headers, n) => {
     try {
-        return await axios
-            .post(
-                'https://translate.api.cloud.yandex.net/translate/v2/translate',
-                body,
-                headers,
-            )
-            .then((response) => {
-                translate = response.data.translations[0].text
-                return translate
-            })
-        // .catch(async (err) => {
-        //     logAlerts('yandex_api_ERROR_translate: ', err)
-
-        //     refreshTokenIAM()
-        //     console.log('yandex_api_ERROR_translate: ')
-        // })
+        const response = await axios.post(url, body, headers)
+        const translate = response.data.translations[0].text
+        return translate
     } catch (err) {
-        console.log('catch_error :>> ', err)
+        console.log('catch_error request_retry :>> ')
         if (n <= 1) throw err
         await sleep(1000)
-        IAM_TOKEN = refreshTokenIAM()
-        return await request_retry({ url, body, headers, n: n - 1 })
+        const newToken = refreshTokenIAM()
+        headers = { headers: { Authorization: `Bearer ${newToken}` } }
+        return request_retry(url, body, headers, n - 1)
     }
 }
 
 module.exports = async function translateText(texts, IAM_TOKEN) {
-    let translate
     const body = {
         sourceLanguageCode: source_language,
         targetLanguageCode: target_language,
@@ -49,25 +35,12 @@ module.exports = async function translateText(texts, IAM_TOKEN) {
     const headers = { headers: { Authorization: `Bearer ${IAM_TOKEN}` } }
     const url = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
 
-    //try 60 seconds
-    translate = request_retry(url, body, headers, 60)
-
-    // translate = await axios
-    //     .post(
-    //         'https://translate.api.cloud.yandex.net/translate/v2/translate',
-    //         body,
-    //         headers,
-    //     )
-    //     .then((response) => {
-    //         translate = response.data.translations[0].text
-    //         return translate
-    //     })
-    //     .catch(async (err) => {
-    //         logAlerts('yandex_api_ERROR_translate: ', err)
-
-    //         refreshTokenIAM()
-    //         console.log('yandex_api_ERROR_translate: ')
-    //     })
-
+    let translate
+    try {
+        translate = await request_retry(url, body, headers, 60)
+    } catch (err) {
+        console.log('yandex_api_ERROR_translate: ')
+        console.error(err)
+    }
     return translate
 }
