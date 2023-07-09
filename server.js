a6eda638e9da421d18109648e4aacb9cf4050d9e
yair-roshal <yair7705778@gmail.com>
2023-07-09 03:26:35 +0300

    fix

--- server.js
+++ server.js
@@ -1,65 +1,67 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv")
 dotenv.config()
-const TelegramBot = require('node-telegram-bot-api')
-const token =
-    process.env.NODE_ENV === 'prod'
-        ? process.env.TELEGRAM_BOT_TOKEN
-        : process.env.TELEGRAM_BOT_TOKEN_testing
+const TelegramBot = require("node-telegram-bot-api")
+const token = process.env.TELEGRAM_BOT_TOKEN
+
+// const token =
+//     process.env.NODE_ENV === 'prod'
+//         ? process.env.TELEGRAM_BOT_TOKEN
+//         : process.env.TELEGRAM_BOT_TOKEN_testing
 
 const bot = new TelegramBot(token, { polling: true })
-const getAllWordsFromFiles = require('./utils/getAllWordsFromFiles.js')
+const getAllWordsFromFiles = require("./utils/getAllWordsFromFiles.js")
 const { dictionaryText } = getAllWordsFromFiles()
 const CHAT_ID_ADMIN = process.env.CHAT_ID_ADMIN
-const { sec, ms, min, interval } = require('./constants/intervals.js')
-const { textMessageHtml } = require('./constants/texts.js')
-const sendingWordMessage = require('./utils/sendingWordMessage.js')
-const dictionaryTextToFile = require('./utils/dictionaryTextToFile.js')
-const { give_me_keyboard } = require('./constants/menus.js')
+const { sec, ms, min, interval } = require("./constants/intervals.js")
+const { textMessageHtml } = require("./constants/texts.js")
+const sendingWordMessage = require("./utils/sendingWordMessage.js")
+const dictionaryTextToFile = require("./utils/dictionaryTextToFile.js")
+const { give_me_keyboard } = require("./constants/menus.js")
 
 //caching dictionaries======
 dictionaryTextToFile()
 // logSessions()
 
 var optionsMessage = {
-    // keyboard=====
-    // reply_markup: JSON.stringify(start_keyboard),
-    parse_mode: 'HTML',
-    //disable because we don't want show description links
-    disable_web_page_preview: true,
+  // keyboard=====
+  // reply_markup: JSON.stringify(start_keyboard),
+  parse_mode: "HTML",
+  //disable because we don't want show description links
+  disable_web_page_preview: true,
 }
 // 1 message!!!!!!
 bot.sendMessage(CHAT_ID_ADMIN, textMessageHtml, optionsMessage)
 
 let dictionary
 if (dictionaryText) {
-    dictionary = dictionaryText.split(/\r?\n/).filter(Boolean)
+  dictionary = dictionaryText.split(/\r?\n/).filter(Boolean)
 }
 
 // callback_query ===============================================
-bot.on('callback_query', (query) => {
-    const chatId = query.from.id
-    // console.log('query ---------------:>> ', query)
+bot.on("callback_query", (query) => {
+  const chatId = query.from.id
+  // console.log('query ---------------:>> ', query)
 
-    if (query.data === 'give_me') {
-        sendingWordMessage(dictionary, bot, chatId)
-    }
+  if (query.data === "give_me") {
+    sendingWordMessage(dictionary, bot, chatId)
+  }
 })
 
 // start ===============================================
 bot.onText(/\/start/, async (msg) => {
-    const chatId = msg.chat.id
-    var photoPath = __dirname + '/media/logo.jpg'
-    // console.log('photoPath :>> ', photoPath)
+  const chatId = msg.chat.id
+  var photoPath = __dirname + "/media/logo.jpg"
+  // console.log('photoPath :>> ', photoPath)
 
-    var optionsMessage = {
-        caption: `Catch the first word, the rest will be in ${min} minutes`,
-        reply_markup: JSON.stringify(give_me_keyboard),
-    }
+  var optionsMessage = {
+    caption: `Catch the first word, the rest will be in ${min} minutes`,
+    reply_markup: JSON.stringify(give_me_keyboard),
+  }
 
-    await bot.sendPhoto(chatId, photoPath, optionsMessage)
+  await bot.sendPhoto(chatId, photoPath, optionsMessage)
 
-    sendingWordMessage(dictionary, bot, chatId)
-    setInterval(() => sendingWordMessage(dictionary, bot, chatId), interval) //  start function by interval
+  sendingWordMessage(dictionary, bot, chatId)
+  setInterval(() => sendingWordMessage(dictionary, bot, chatId), interval) //  start function by interval
 })
 
 // sending a list of words and adding them to the dictionary ===============
@@ -87,7 +89,7 @@ bot.onText(/\/start/, async (msg) => {
 
 //===============
 
-console.log('server started with interval:', interval / ms / sec, 'min')
+console.log("server started with interval:", interval / ms / sec, "min")
 
 // {
 //     process.env.NODE_ENV === 'dev' &&
