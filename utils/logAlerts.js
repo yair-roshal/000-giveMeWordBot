36836423630d31872293ef496aeb50222cffb3a8
yair-roshal <yair7705778@gmail.com>
2023-07-10 20:43:56 +0300

    fix

--- utils/logAlerts.js
+++ utils/logAlerts.js
@@ -1,18 +1,41 @@
-let fs = require('fs')
-let appendFile = require('fs')
-const getPathToFolder = require('./getPathToFolder')
-let path = require('path')
+// let fs = require('fs')
+// let appendFile = require('fs')
+// const getPathToFolder = require('./getPathToFolder')
+// let path = require('path')
+
+// module.exports = function logAlerts(alert) {
+//     console.log('logAlerts :>>>>>>>>>>>>>>>>> !!!!!!!!!!!!!!!!!!')
+//     console.log('alert :>> ', alert)
+//     let nameFile = 'log-alerts.txt'
+//     let pathFile = getPathToFolder('data/logs/') + `${nameFile}`
+
+//     fs.appendFile(pathFile, alert + '\r\n', (err) => {
+//         if (!err) {
+//         } else {
+//             console.log(err)
+//         }
+//     })
+// }
+
+
+const fs = require('fs');
+const path = require('path');
+const getPathToFolder = require('./getPathToFolder');
 
 module.exports = function logAlerts(alert) {
-    console.log('logAlerts :>>>>>>>>>>>>>>>>> !!!!!!!!!!!!!!!!!!')
-    console.log('alert :>> ', alert)
-    let nameFile = 'log-alerts.txt'
-    let pathFile = getPathToFolder('data/logs/') + `${nameFile}`
+    console.log('logAlerts :>>>>>>>>>>>>>>>>> !!!!!!!!!!!!!!!!!!');
+    console.log('alert :>> ', alert);
+    const nameFile = 'log-alerts.txt';
+    const folderPath = getPathToFolder('data/logs/');
+    const pathFile = path.join(folderPath, nameFile);
+
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+    }
 
     fs.appendFile(pathFile, alert + '\r\n', (err) => {
-        if (!err) {
-        } else {
-            console.log(err)
+        if (err) {
+            console.log(err);
         }
-    })
-}
+    });
+};
