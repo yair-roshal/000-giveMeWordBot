ff89cef650f04dd51448b0d83da35273a7adcb08
yair-roshal <yair7705778@gmail.com>
2023-07-11 11:15:15 +0300

    fix

--- utils/getTokenJWT.js
+++ utils/getTokenJWT.js
@@ -7,13 +7,14 @@ const serviceAccountId = process.env.SERVICE_ACCOUNT_ID
 const keyId = process.env.KEY_ID
 const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
 const now = Math.floor(new Date().getTime() / 1000)
+const jwtExpirationTimeout = 3600 * 1000
 
 const getTokenJWT = async () => {
     const payload = {
         aud: 'https://iam.api.cloud.yandex.net/iam/v1/tokens',
         iss: serviceAccountId,
         iat: now,
-        exp: now + 3600,
+        exp: now + jwtExpirationTimeout,
     }
 
     const key = await jose.JWK.asKey(privateKey, 'pem', {
--- utils/refreshTokenIAM.js
+++ utils/refreshTokenIAM.js
@@ -4,26 +4,27 @@ const getTokenJWT = require("./getTokenJWT")
 // const changeTokenToIAM = require('./changeTokenToIAM')
 
 module.exports = async function refreshTokenIAM() {
-  console.log("refreshTokenIAM")
   const tokenJWT = await getTokenJWT()
+  console.log("refreshTokenIAM_tokenJWT", tokenJWT)
 
   let jwtObj = {
     jwt: tokenJWT,
   }
 
-  console.log('jwtObj :>> ', jwtObj)
-  try {
-    const result = await axios
-      .post("https://iam.api.cloud.yandex.net/iam/v1/tokens", jwtObj)
-      .then((response) => {
-        let IAM_TOKEN = response.data.iamToken
-        console.log("IAM_TOKEN==", Boolean(IAM_TOKEN))
-        // console.log('IAM_TOKEN==', { IAM_TOKEN })
-        return IAM_TOKEN
-      })
-    return result
-  } catch (err) {
-    // logAlerts(err)
-    console.log("AXIOS ERROR _ refreshTokenIAM: ", err.response)
-  }
+  return changeTokenToIAM(jwtObj)
+
+  // try {
+  //   const result = await axios
+  //     .post("https://iam.api.cloud.yandex.net/iam/v1/tokens", jwtObj)
+  //     .then((response) => {
+  //       let IAM_TOKEN = response.data.iamToken
+  //       console.log("IAM_TOKEN==", Boolean(IAM_TOKEN))
+  //       // console.log('IAM_TOKEN==', { IAM_TOKEN })
+  //       return IAM_TOKEN
+  //     })
+  //   return result
+  // } catch (err) {
+  //   // logAlerts(err)
+  //   console.log("AXIOS ERROR _ refreshTokenIAM: ", err.response)
+  // }
 }
