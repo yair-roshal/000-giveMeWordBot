4ddf48944715fa4b83377f4535b2f822bb817117
yair-roshal <yair7705778@gmail.com>
2023-07-17 19:39:31 +0300

    fix

--- utils/getTokenJWT.js
+++ utils/getTokenJWT.js
@@ -1,13 +1,14 @@
 const dotenv = require("dotenv")
 dotenv.config()
-const formatDate = require('./formatDate.js')
+const formatDate = require("./formatDate.js")
 
 const jose = require("node-jose")
 
 const serviceAccountId = process.env.SERVICE_ACCOUNT_ID
 const keyId = process.env.KEY_ID
 const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
-const now = Math.floor(new Date().getTime() / 1000)
+// const now = Math.floor(new Date().getTime() / 1000)
+const now = new Date().getTime()
 const jwtExpirationTimeout = 3600
 
 const getTokenJWT = async () => {
@@ -18,7 +19,6 @@ const getTokenJWT = async () => {
     exp: now + jwtExpirationTimeout,
   }
 
-
   const key = await jose.JWK.asKey(privateKey, "pem", {
     kid: keyId,
     alg: "PS256",
@@ -28,11 +28,10 @@ const getTokenJWT = async () => {
     .update(JSON.stringify(payload))
     .final()
 
-    console.log("getTokenJWT_now", formatDate(payload?.iat))
-    console.log("getTokenJWT_exp", formatDate(payload?.exp))
-    console.log("token", token)
+  console.log("getTokenJWT_now", formatDate(payload?.iat))
+  console.log("getTokenJWT_exp", formatDate(payload?.exp))
+  console.log("token", token)
 
-    
   return token
 }
 
