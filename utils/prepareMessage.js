afc1d82623947601fcc9725043dcca024d885180
yair-roshal <yair7705778@gmail.com>
2023-09-03 00:43:19 +0300

    fix

--- utils/prepareMessage.js
+++ utils/prepareMessage.js
@@ -45,7 +45,7 @@ module.exports = async function prepareMessage(
           examples +=
             "\r\n" +
             `<b>- ${responseData[0].meanings[key0].definitions[key].example}</b>`
- 
+
           await translateText(
             responseData[0].meanings[key0].definitions[key].example,
             IAM_TOKEN
@@ -103,9 +103,7 @@ module.exports = async function prepareMessage(
 
     const linkToTranslate = `https://context.reverso.net/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/${firstWord}`
 
-    return `<b>_______________________________</b>
-        
-<b>${phoneticLine}${wordLineDictionary} </b>
+    return `<b>${phoneticLine}${wordLineDictionary} </b>
 ${examplesLine}
 <b>${randomIndex + 1}/(${dictionaryLength}) </b>
 
