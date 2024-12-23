dedc91e1ff4f97f5eb3857810cd0f4f92be736f8
yair-roshal <yair7705778@gmail.com>
2024-12-23 21:24:05 +0200

    build(deps): update package-lock.json, package.json and server.js

--- server.js
+++ server.js
@@ -2,127 +2,137 @@ const dotenv = require("dotenv")
 dotenv.config()
 const TelegramBot = require("node-telegram-bot-api")
 const { clockStart, clockEnd } = require("./constants/intervals.js")
-const getAllWordsFromFiles = require("./utils/getAllWordsFromFiles.js")
-const { dictionaryText } = getAllWordsFromFiles()
+// const getAllWordsFromFiles = require("./utils/getAllWordsFromFiles.js")
+// const { dictionaryText } = getAllWordsFromFiles()
 const { sec, ms, min, interval } = require("./constants/intervals.js")
 const { textMessageHtml } = require("./constants/texts.js")
 const sendingWordMessage = require("./utils/prepareMessage.js")
 const dictionaryTextToFile = require("./utils/dictionaryTextToFile.js")
 const { give_me_keyboard } = require("./constants/menus.js")
 
-// const token =
-//     process.env.NODE_ENV === 'prod'
-//         ? process.env.TELEGRAM_BOT_TOKEN
-//         : process.env.TELEGRAM_BOT_TOKEN_testing
+const getWordsFromGoogleDocs = require("./utils/getWordsFromGoogleDocs.js")
 
-const token = process.env.TELEGRAM_BOT_TOKEN
-console.log('token :>> ', token);
-const bot = new TelegramBot(token, { polling: true })
+async function startBot() {
+  // const token =
+  //     process.env.NODE_ENV === 'prod'
+  //         ? process.env.TELEGRAM_BOT_TOKEN
+  //         : process.env.TELEGRAM_BOT_TOKEN_testing
 
-//caching dictionaries======
-dictionaryTextToFile()
-// logSessions()
+  const token = process.env.TELEGRAM_BOT_TOKEN
+  console.log("token :>> ", token)
+  const bot = new TelegramBot(token, { polling: true })
 
-var optionsMessage = {
-  // keyboard=====
-  // reply_markup: JSON.stringify(start_keyboard),
-  parse_mode: "HTML",
-  //disable because we don't want show description links
-  disable_web_page_preview: true,
-}
-// 1 message!!!!!!
-console.log("send_start_message")
-
-const CHAT_ID_ADMIN = process.env.CHAT_ID_ADMIN
-bot.sendMessage(CHAT_ID_ADMIN, textMessageHtml, optionsMessage)
-
-let dictionary
-if (dictionaryText) {
-  dictionary = dictionaryText.split(/\r?\n/).filter(Boolean)
-}
-
-// callback_query ===============================================
-bot.on("callback_query", (query) => {
-  const chatId = query.from.id
-  // console.log('query ---------------:>> ', query)
-
-  if (query.data === "give_me") {
-    sendingWordMessage(dictionary, bot, chatId)
-  }
-})
-
-// start ===============================================
-bot.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id
-  var photoPath = __dirname + "/media/logo.jpg"
-  // console.log('photoPath :>> ', photoPath)
+  //caching dictionaries======
+  // dictionaryTextToFile()
+  // logSessions()
 
   var optionsMessage = {
-    caption: `Catch the first word, the rest will be in ${min} minutes`,
-    reply_markup: JSON.stringify(give_me_keyboard),
+    // keyboard=====
+    // reply_markup: JSON.stringify(start_keyboard),
+    parse_mode: "HTML",
+    //disable because we don't want show description links
+    disable_web_page_preview: true,
+  }
+  // 1 message!!!!!!
+  console.log("send_start_message")
+
+  const CHAT_ID_ADMIN = process.env.CHAT_ID_ADMIN
+  bot.sendMessage(CHAT_ID_ADMIN, textMessageHtml, optionsMessage)
+
+  const dictionaryText = await getWordsFromGoogleDocs()
+
+  console.log("dictionaryText", dictionaryText)
+
+  let dictionary
+  if (dictionaryText) {
+    dictionary = dictionaryText.split(/\r?\n/).filter(Boolean)
   }
 
-  await bot.sendPhoto(chatId, photoPath, optionsMessage)
+  // callback_query ===============================================
+  bot.on("callback_query", (query) => {
+    const chatId = query.from.id
+    // console.log('query ---------------:>> ', query)
 
-  sendingWordMessage(dictionary, bot, chatId)
+    if (query.data === "give_me") {
+      sendingWordMessage(dictionary, bot, chatId)
+    }
+  })
 
-  setInterval(
-    () => {
-      let isTimeForSending = false
+  // start ===============================================
+  bot.onText(/\/start/, async (msg) => {
+    const chatId = msg.chat.id
+    var photoPath = __dirname + "/media/logo.jpg"
+    // console.log('photoPath :>> ', photoPath)
 
-      let currentDate = new Date()
-      let nowHours = currentDate.getHours()
-      let nowMinutes = currentDate.getMinutes()
+    var optionsMessage = {
+      caption: `Catch the first word, the rest will be in ${min} minutes`,
+      reply_markup: JSON.stringify(give_me_keyboard),
+    }
 
-      if (process.env.NODE_ENV === "dev") {
-        isTimeForSending = true
-      } else if (nowHours < clockEnd && nowHours > clockStart) {
-        isTimeForSending = true
-      } else {
-        console.log(
-          `it isn't time for sending messages  -   ${nowHours}:${nowMinutes}`
-        )
-      }
+    await bot.sendPhoto(chatId, photoPath, optionsMessage)
 
-      isTimeForSending && sendingWordMessage(dictionary, bot, chatId)
-    },
+    sendingWordMessage(dictionary, bot, chatId)
 
-    interval
-  )
-})
+    setInterval(
+      () => {
+        let isTimeForSending = false
 
-// sending a list of words and adding them to the dictionary ===============
+        let currentDate = new Date()
+        let nowHours = currentDate.getHours()
+        let nowMinutes = currentDate.getMinutes()
 
-// bot.on('message', (msg) => {
-//     console.log('msg.text===', msg.text)
-//     const chatId = msg.chat.id
+        if (process.env.NODE_ENV === "dev") {
+          isTimeForSending = true
+        } else if (nowHours < clockEnd && nowHours > clockStart) {
+          isTimeForSending = true
+        } else {
+          console.log(
+            `it isn't time for sending messages  -   ${nowHours}:${nowMinutes}`
+          )
+        }
 
-//     bot.sendMessage(
-//         chatId,
-//         'Hello, ' +
-//             msg.chat.first_name +
-//             '! Please choose what you want from menu',
-//         start_inline_keyboard,
-//     )
+        isTimeForSending && sendingWordMessage(dictionary, bot, chatId)
+      },
 
-//     // else if (!dictionary.includes(msg.text) && msg.text !== '/start') {
-//     //     dictionary = dictionary.concat(msg.text.split(/\r?\n/))
-//     //     bot.sendMessage(
-//     //         chatId,
-//     //         `Successfully added "${msg.text}" to the dictionary.`,
-//     //     )
-//     // }
-// })
+      interval
+    )
+  })
 
-//===============
+  // sending a list of words and adding them to the dictionary ===============
 
-console.log("server started with interval:", interval / ms / sec, "min")
+  // bot.on('message', (msg) => {
+  //     console.log('msg.text===', msg.text)
+  //     const chatId = msg.chat.id
 
-// {
-//     process.env.NODE_ENV === 'dev' &&
-//         sendingWordMessage(dictionary, bot, CHAT_ID_ADMIN) &&
-//         setInterval(
-//             () => sendingWordMessage(dictionary, bot, CHAT_ID_ADMIN),
-//             interval,
-//         )
-// }
+  //     bot.sendMessage(
+  //         chatId,
+  //         'Hello, ' +
+  //             msg.chat.first_name +
+  //             '! Please choose what you want from menu',
+  //         start_inline_keyboard,
+  //     )
+
+  //     // else if (!dictionary.includes(msg.text) && msg.text !== '/start') {
+  //     //     dictionary = dictionary.concat(msg.text.split(/\r?\n/))
+  //     //     bot.sendMessage(
+  //     //         chatId,
+  //     //         `Successfully added "${msg.text}" to the dictionary.`,
+  //     //     )
+  //     // }
+  // })
+
+  //===============
+
+  console.log("server started with interval:", interval / ms / sec, "min")
+
+  // {
+  //     process.env.NODE_ENV === 'dev' &&
+  //         sendingWordMessage(dictionary, bot, CHAT_ID_ADMIN) &&
+  //         setInterval(
+  //             () => sendingWordMessage(dictionary, bot, CHAT_ID_ADMIN),
+  //             interval,
+  //         )
+  // }
+}
+
+startBot()
---
+++ utils/getWordsFromGoogleDocs.js
@@ -0,0 +1,24 @@
+const axios = require("axios")
+
+module.exports = async function fetchGoogleDocText() {
+  // Идентификатор документа (из ссылки)
+  const documentId = "167xQkssbS9dBMwLBaU0jD7FTXrTqJYbzr7YBmh-OGk8"
+
+  // URL для экспорта содержимого в формате текста
+  const exportUrl = `https://docs.google.com/document/d/${documentId}/export?format=txt`
+
+  try {
+    // Делаем запрос к публичному документу
+    const response = await axios.get(exportUrl)
+
+    if (response.status === 200) {
+      // Выводим содержимое документа
+      console.log("Document content:\n", response.data)
+      return response.data
+    } else {
+      console.log(`Ошибка: статус ${response.status}`)
+    }
+  } catch (error) {
+    console.error("Ошибка при запросе документа:", error.message)
+  }
+}
