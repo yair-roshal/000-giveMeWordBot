c2d132885ec44ec652f0561de520e31c30b14646
yair-roshal <yair7705778@gmail.com>
2024-12-23 21:30:43 +0200

    update getPathToFolder.js

--- utils/getPathToFolder.js
+++ utils/getPathToFolder.js
@@ -1,12 +1,16 @@
 let path = require('path')
 
 module.exports = function getPathToFolder(folder) {
-    if (process.env.NODE_ENV === 'dev') {
-        return path.join(__dirname, `../${folder}`)
-    }
+    
+    return path.join(__dirname, `../${folder}`)
+    
+    
+    // if (process.env.NODE_ENV === 'dev') {
+    //     return path.join(__dirname, `../${folder}`)
+    // }
 
-    if (process.env.NODE_ENV === 'prod') {
-        console.log('prod!!!')
-        return path.join(__dirname, `../${folder}`)
-    }
+    // if (process.env.NODE_ENV === 'prod') {
+    //     console.log('prod!!!')
+    //     return path.join(__dirname, `../${folder}`)
+    // }
 }
